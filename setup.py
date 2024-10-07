2114c0ce97acd607f1508fdc22f924ba1f3c777e
Maestoz <S.A.Roz@yandex.ru>
2024-10-08 00:25:15 +0700

    add version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def find_required():
 
 setup(
     name="vedro-spec-validator",
-    version="0.0.1",
+    version="0.0.3",
     description="Vedro Spec Validator plugin",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
